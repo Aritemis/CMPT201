0b67b9aa98a529465db4dbd2217cc21b8d49275a
Ariana Fairbanks <arianafairbanks@gmail.com>
2018-09-27 17:59:33 -0600

    lab7

---
+++ src/lab7_mazes/Maze.java
@@ -0,0 +1,181 @@
+/**
+ * @author Ariana Fairbanks
+ */
+
+package lab7_mazes;
+
+public class Maze extends MazeSolver
+{
+	private State position;
+	private int[][] map;
+	private int startRow;
+	private int startCol;
+	private boolean showSteps;
+
+	public Maze(String fileName, int numberOfRows, int numberOfColumns, boolean showSteps) 
+	{
+		super(fileName, numberOfRows, numberOfColumns);
+		position = startState;
+		startRow = startState.getRow();
+		startCol = startState.getColumn();
+		this.showSteps = showSteps;
+		makeMap();
+	}
+
+	@Override
+	public void solveDFS()
+	{
+		printMaze();
+		position = startState;
+		Stack<State> forks = new ListStack<State>();
+		forks.push(position);
+		
+		while(!position.equals(goalState))
+		{
+			position = forks.pop();
+//			System.out.println(position);
+			int row = position.getRow();
+			int col = position.getColumn();
+
+			if (!isWallOrOldLocation(row, col))
+			{
+				addVisit(position);
+				
+				if(showSteps)
+				{
+					System.out.println("");
+					printMaze();
+				}
+				
+				try
+				{
+					if (!isWallOrOldLocation(row + 1, col))
+					{
+						int newRow = row + 1;
+						forks.push(states[newRow][col]);
+					}
+					if (!isWallOrOldLocation(row - 1, col))
+					{
+						int newRow = row - 1;
+						forks.push(states[newRow][col]);
+					}
+					if (!isWallOrOldLocation(row, col + 1))
+					{
+						int newCol = col + 1;
+						forks.push(states[row][newCol]);
+					}
+					if (!isWallOrOldLocation(row, col - 1))
+					{
+						int newCol = col - 1;
+						forks.push(states[row][newCol]);
+					}
+				}
+				catch(IndexOutOfBoundsException e){}
+			}
+		}
+
+		int row = position.getRow();
+		int col = position.getColumn();
+		states[row][col].setValue('V');
+		System.out.println("");
+		printMaze();
+		System.out.println("");
+		System.out.println("");
+
+	}
+
+	@Override
+	public void solveBFS() 
+	{
+		printMaze();
+		position = startState;
+		Queue<State> forks = new ArrayQueue<State>();
+		forks.add(position);
+		
+		while(!position.equals(goalState))
+		{
+			position = forks.remove();
+//			System.out.println(position);
+			int row = position.getRow();
+			int col = position.getColumn();
+
+			if (!isWallOrOldLocation(row, col))
+			{
+				addVisit(position);
+				
+				if(showSteps)
+				{
+					System.out.println("");
+					printMaze();
+				}
+				
+				try
+				{
+					if (!isWallOrOldLocation(row + 1, col))
+					{
+						int newRow = row + 1;
+						forks.add(states[newRow][col]);
+					}
+					if (!isWallOrOldLocation(row - 1, col))
+					{
+						int newRow = row - 1;
+						forks.add(states[newRow][col]);
+					}
+					if (!isWallOrOldLocation(row, col + 1))
+					{
+						int newCol = col + 1;
+						forks.add(states[row][newCol]);
+					}
+					if (!isWallOrOldLocation(row, col - 1))
+					{
+						int newCol = col - 1;
+						forks.add(states[row][newCol]);
+					}
+				}
+				catch(IndexOutOfBoundsException e){}
+			}
+		}
+
+		int row = position.getRow();
+		int col = position.getColumn();
+		states[row][col].setValue('V');
+		System.out.println("");
+		printMaze();
+		System.out.println("");
+		System.out.println("");
+	}
+	
+	private void makeMap()
+	{
+		map = new int[states.length][states[0].length];
+		for(int row = 0; row < states.length; row++)
+		{
+			for(int col = 0; col < states[row].length; col++)
+			{
+				if(states[row][col].isWall())
+				{
+					map[row][col] = 1;
+				}
+			}
+		}
+	}
+	
+	private boolean isWallOrOldLocation(int row, int col)
+	{
+		boolean isOne = false;
+		if(map[row][col] != 0)
+		{
+			isOne = true;
+		}
+		return isOne;
+	}
+	
+	private void addVisit(State state)
+	{
+		int row = state.getRow();
+		int col = state.getColumn();
+		map[row][col] = 1;
+		states[row][col].setValue('+');
+		states[startRow][startCol].setValue('S');
+	}
+}
---
+++ src/lab7_mazes/TestQueue.java
@@ -0,0 +1,116 @@
+/**
+ * Unit tests to test implementation of queue interface.
+ * @author Ariana Fairbanks
+ */
+
+package lab7_mazes;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestQueue
+{
+
+	@Test
+	public void testListQueue() 
+	{
+		Queue<String> q = new ListQueue<String>();
+		
+		assertTrue(q.isEmpty());
+		assertEquals(0,q.size());
+		assertNull(q.remove());
+		
+		q.add("apple");
+		q.add("banana");
+		q.add("cherry");
+		q.add("date");
+		
+		assertFalse(q.isEmpty());
+		assertEquals(4,q.size());
+		
+		assertEquals("apple", q.remove());
+		assertEquals("banana", q.remove());
+		assertEquals("cherry", q.remove());
+		assertEquals("date", q.remove());
+		
+		assertTrue(q.isEmpty());
+		assertEquals(0,q.size());
+		assertNull(q.remove());
+	}
+	
+	@Test
+	public void testArrayQueue() 
+	{
+		Queue<String> q = new ArrayQueue<String>(6);
+		
+		q.add("apple");
+		q.add("banana");
+		q.add("cherry");
+		q.add("donut");
+		q.add("eggplant");
+		
+		assertEquals("apple", q.remove());
+		assertEquals("banana", q.remove());
+		
+		q.add("fig");
+		q.add("grape");
+		q.add("honey");
+		
+		assertEquals("cherry", q.remove());
+		assertEquals("donut", q.remove());
+		assertEquals("eggplant", q.remove());
+		assertEquals("fig", q.remove());
+		assertEquals("grape", q.remove());
+		assertEquals("honey", q.remove());
+		
+		
+	//check ensure capacity methods that act when there is only one index left in the array
+		Queue<String> n = new ArrayQueue<String>(7);
+		
+		n.add("apple");
+		n.add("banana");
+		n.add("cherry");
+		n.add("donut");
+		n.add("eggplant");
+		
+		assertEquals("apple", n.remove());
+		assertEquals("banana", n.remove());
+		
+		n.add("fig");
+		n.add("grape");
+		n.add("honey");
+		
+		assertEquals("cherry", n.remove());
+		assertEquals("donut", n.remove());
+		assertEquals("eggplant", n.remove());
+		assertEquals("fig", n.remove());
+		assertEquals("grape", n.remove());
+		assertEquals("honey", n.remove());
+		
+	//check ensure capacity methods that act when the string is full
+		@SuppressWarnings("unused")
+		Queue<String> p = new ArrayQueue<String>(5);
+		
+		q.add("apple");
+		q.add("banana");
+		q.add("cherry");
+		q.add("donut");
+		q.add("eggplant");
+		
+		assertEquals("apple", q.remove());
+		assertEquals("banana", q.remove());
+		
+		q.add("fig");
+		q.add("grape");
+		q.add("honey");
+		
+		assertEquals("cherry", q.remove());
+		assertEquals("donut", q.remove());
+		assertEquals("eggplant", q.remove());
+		assertEquals("fig", q.remove());
+		assertEquals("grape", q.remove());
+		assertEquals("honey", q.remove());
+		
+	}
+
+}
