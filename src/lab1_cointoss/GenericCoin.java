e7a415b23a3d03794451db0154bc8ac0b3e8bc36
Ariana Fairbanks <arianafairbanks@gmail.com>
2018-09-27 17:18:12 -0600

    Initial commit

---
+++ src/lab1_cointoss/GenericCoin.java
@@ -0,0 +1,76 @@
+/**
+ * @name Ariana Fairbanks
+ * @name Gabriel Rusk
+ * @date January 2017
+ * 
+ * This class provides a generic coin.
+ */
+
+package lab1_cointoss;
+
+public class GenericCoin
+{
+
+	public enum CoinSide
+	{
+		HEADS, TAILS
+	};
+	private CoinSide side;
+
+	public GenericCoin()
+	{
+		side = CoinSide.HEADS;
+	}
+
+	public void setToHeads()
+	{
+		this.side = CoinSide.HEADS;
+	}
+
+	public void setToTails()
+	{
+		this.side = CoinSide.TAILS;
+	}
+
+	public void toss()
+	{
+		int rng = (int) (Math.random() * 10);
+		if (rng < 6)
+		{
+			this.side = CoinSide.HEADS;
+		}
+		else
+		{
+			this.side = CoinSide.TAILS;
+		}
+	}
+
+	public boolean isHeads()
+	{
+		boolean heads = false;
+		if (this.side == CoinSide.HEADS)
+		{
+			heads = true;
+		}
+		return heads;
+	}
+
+	public boolean isTails()
+	{
+		boolean tails = false;
+		if (this.side == CoinSide.TAILS)
+		{
+			tails = true;
+		}
+		return tails;
+	}
+
+	public String toString()
+	{
+		if (this.side == CoinSide.HEADS)
+		{
+			return "heads";
+		}
+		return "tails";
+	}
+}
